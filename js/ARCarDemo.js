15b04e76ab0d49bf296850b4e9d8755c981a92f2
Vivien Ngo <vivienhngo@gmail.com>
2019-02-10 02:20:26 -0500

    Add white planet on -z axis

--- js/ARCarDemo.js
+++ js/ARCarDemo.js
@@ -32,9 +32,27 @@ var ARCarDemo = createReactClass({
       tapGrey: false,
       tapRed: false,
       tapYellow: false,
+      rotationPiv: [0,0,0],
+      orbitAnim: false
     }
   },
+              // sphere 1
+              // onClick={this._selectGrey}
+              // animation={{name:"tapAnimation", run:this.state.tapGrey, onFinish:this._animateFinished}}
+              // 
+              // sphere 2
+              // animation={{name:"orbit", run:true, loop:true}}
 
+
+            // <ViroSphere materials={["blue_sphere"]}
+            //   heightSegmentCount={20} widthSegmentCount={20} radius={.07}
+            //   position={[0, -0.5, 0]}
+            //   shadowCastingBitMask={0} />
+
+            // <ViroSphere materials={["red_sphere"]}
+            //   heightSegmentCount={20} widthSegmentCount={20} radius={.07}
+            //   position={[-0.5, 0, 0]}
+            //   shadowCastingBitMask={0} />
   render: function() {
     return (
       <ViroARScene>
@@ -42,26 +60,31 @@ var ARCarDemo = createReactClass({
         <ViroLightingEnvironment source={require('./res/tesla/garage_1k.hdr')}/>
 
         <ViroARImageMarker target={"logo"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
-          <ViroNode scale={[0, 0, 0]} transformBehaviors={["billboardY"]} animation={{name:"scaleSphereUp", run:this.state.animateCar,}}>
+          <ViroNode scale={[0, 0, 0]} rotationPivot={this.state.rotationPiv} transformBehaviors={["billboardY"]} animation={{name:"scaleSphereUp", run:this.state.animateCar,}}>
             <ViroSphere materials={["yellow_sphere"]}
               heightSegmentCount={20} widthSegmentCount={20} radius={.1}
               position={[0, 0, 0]}
               shadowCastingBitMask={0} />
 
-            <ViroSphere materials={["blue_sphere"]}
-              heightSegmentCount={20} widthSegmentCount={20} radius={.005}
-              position={[0, -.1, 0]}
-              animation={{name:"orbit", run:true, loop:true}}
+            <ViroSphere materials={["white_sphere"]}
+              heightSegmentCount={20} widthSegmentCount={20} radius={.07}
+              position={[0, 0, -0.5]}
+              animation={{name:"orbit", run:this.state.orbitAnim, loop:true}}
               shadowCastingBitMask={0} />
           </ViroNode>
         </ViroARImageMarker>
       </ViroARScene>
     );
   },
-  _onAnchorFound() {
+  _onAnchorFound(anchor) {
     this.setState({
       animateCar: true,
+      rotationPiv: anchor.position,
+      orbitAnim: true
     })
+
+    console.log('hi')
+    console.log(anchor)
   },
 });
 
@@ -89,15 +112,26 @@ ViroMaterials.createMaterials({
 });
 
 ViroARTrackingTargets.createTargets({
-  logo : {
+  "logo" : {
     source : require('./res/logo.png'),
     orientation : "Up",
-    physicalWidth : 0.165 // real world width in meters
-  }
+    physicalWidth : 0.150 // real world width in meters
+  },
+  "poster" : {
+    source : require('./res/blackpanther.jpg'),
+    orientation : "Up",
+    physicalWidth : 0.6096 // real world width in meters
+  },
+  "lettera" : {
+    source : require('./res/letterA.png'),
+    orientation : "Up",
+    physicalWidth : 0.1 // real world width in meters
+  },
 });
 
 ViroAnimations.registerAnimations({
-    orbit:{properties:{rotateZ:"+=45"}, duration:1000}, //add 45 degrees to the y angle of the component every 1 second
+    orbit:{properties:{rotateY:"+=45"},
+                  duration:1000}, //add 45 degrees to the y angle of the component every 1 second
     scaleUp:{properties:{scaleX:1, scaleY:1, scaleZ:1,},
                   duration: 500, easing: "bounce"},
     scaleDown:{properties:{scaleX:0, scaleY:0, scaleZ:0,},
