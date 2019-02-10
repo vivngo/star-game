3e5a1c124d0a7b185a50fa7d16a82014dd9ad394
Vivien Ngo <vivienhngo@gmail.com>
2019-02-10 00:14:45 -0500

    Add planet

--- js/ARCarDemo.js
+++ js/ARCarDemo.js
@@ -46,8 +46,12 @@ var ARCarDemo = createReactClass({
             <ViroSphere materials={["yellow_sphere"]}
               heightSegmentCount={20} widthSegmentCount={20} radius={.1}
               position={[0, 0, 0]}
-              onClick={this._selectGrey}
-              animation={{name:"tapAnimation", run:this.state.tapGrey, onFinish:this._animateFinished}}
+              shadowCastingBitMask={0} />
+
+            <ViroSphere materials={["blue_sphere"]}
+              heightSegmentCount={20} widthSegmentCount={20} radius={.005}
+              position={[0, -.1, 0]}
+              animation={{name:"orbit", run:true, loop:true}}
               shadowCastingBitMask={0} />
           </ViroNode>
         </ViroARImageMarker>
@@ -59,84 +63,9 @@ var ARCarDemo = createReactClass({
       animateCar: true,
     })
   },
-  _toggleButtons() {
-    this.setState({
-      animName: (this.state.animName == "scaleUp" ? "scaleDown" : "scaleUp"),
-      playAnim: true
-    })
-  },
-  _selectWhite(){
-    this.setState({
-      texture : "white",
-      tapWhite: true
-    })
-  },
-  _selectBlue(){
-    this.setState({
-      texture : "blue",
-      tapBlue: true
-    })
-  },
-  _selectGrey(){
-    this.setState({
-      texture : "grey",
-      tapGrey: true
-    })
-  },
-  _selectRed(){
-    this.setState({
-      texture : "red",
-      tapRed: true
-    })
-  },
-  _selectYellow(){
-    this.setState({
-      texture : "yellow",
-      tapYellow: true
-    })
-  },
-  _animateFinished(){
-    this.setState({
-      tapWhite: false,
-      tapBlue: false,
-      tapGrey: false,
-      tapRed: false,
-      tapYellow: false,
-    })
-  },
 });
 
 ViroMaterials.createMaterials({
-  white: {
-    lightingModel: "PBR",
-    diffuseTexture: require('./res/tesla/object_car_main_Base_Color.png'),
-    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
-    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
-  },
-  blue: {
-    lightingModel: "PBR",
-    diffuseTexture: require('./res/tesla/object_car_main_Base_Color_blue.png'),
-    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
-    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
-  },
-  grey: {
-    lightingModel: "PBR",
-    diffuseTexture: require('./res/tesla/object_car_main_Base_Color_grey.png'),
-    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
-    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
-  },
-  red: {
-    lightingModel: "PBR",
-    diffuseTexture: require('./res/tesla/object_car_main_Base_Color_red.png'),
-    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
-    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
-  },
-  yellow: {
-    lightingModel: "PBR",
-    diffuseTexture: require('./res/tesla/object_car_main_Base_Color_yellow.png'),
-    metalnessTexture: require('./res/tesla/object_car_main_Metallic.png'),
-    roughnessTexture: require('./res/tesla/object_car_main_Roughness.png'),
-  },
   white_sphere: {
     lightingModel: "PBR",
     diffuseColor: "rgb(231,231,231)",
@@ -168,12 +97,11 @@ ViroARTrackingTargets.createTargets({
 });
 
 ViroAnimations.registerAnimations({
+    orbit:{properties:{rotateZ:"+=45"}, duration:1000}, //add 45 degrees to the y angle of the component every 1 second
     scaleUp:{properties:{scaleX:1, scaleY:1, scaleZ:1,},
                   duration: 500, easing: "bounce"},
     scaleDown:{properties:{scaleX:0, scaleY:0, scaleZ:0,},
                   duration: 200,},
-    scaleCar:{properties:{scaleX:.09, scaleY:.09, scaleZ:.09,},
-                  duration: 500, easing: "bounce"},
     scaleSphereUp:{properties:{scaleX:.8, scaleY:.8, scaleZ:.8,},
                   duration: 50, easing: "easeineaseout"},
     scaleSphereDown:{properties:{scaleX:1, scaleY:1, scaleZ:1,},
