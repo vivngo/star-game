49c401d6193477865dbb4a781e3b99d9750d83c9
Peggy Li <xpeggyxli@gmail.com>
2019-02-09 23:59:30 -0500

    Increase sphere size

--- js/ARCarDemo.js
+++ js/ARCarDemo.js
@@ -44,33 +44,12 @@ var ARCarDemo = createReactClass({
         <ViroARImageMarker target={"logo"} onAnchorFound={this._onAnchorFound} pauseUpdates={this.state.pauseUpdates}>
           <ViroNode scale={[0, 0, 0]} transformBehaviors={["billboardY"]} animation={{name:"scaleSphereUp", run:this.state.animateCar,}}>
             <ViroSphere materials={["yellow_sphere"]}
-              heightSegmentCount={20} widthSegmentCount={20} radius={.03}
+              heightSegmentCount={20} widthSegmentCount={20} radius={.1}
               position={[0, 0, 0]}
               onClick={this._selectGrey}
               animation={{name:"tapAnimation", run:this.state.tapGrey, onFinish:this._animateFinished}}
               shadowCastingBitMask={0} />
           </ViroNode>
-
-
-
-          <ViroSpotLight
-            innerAngle={5}
-            outerAngle={25}
-            direction={[0,-1,0]}
-            position={[0, 5, 1]}
-            color="#ffffff"
-            castsShadow={true}
-            shadowMapSize={2048}
-            shadowNearZ={2}
-            shadowFarZ={7}
-            shadowOpacity={.7} />
-
-          <ViroQuad
-            rotation={[-90, 0, 0]}
-            position={[0, -0.001, 0]}
-            width={2.5} height={2.5}
-            arShadowReceiver={true} />
-
         </ViroARImageMarker>
       </ViroARScene>
     );
